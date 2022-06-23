98b47fafd8b52d200b8021d6cacd99fea607ae49
BrunoBGDev <bruno.braga.design@gmail.com>
2022-06-23 14:05:49 -0300

    Update 0.0.1

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 13:53:05 by bbraga            #+#    #+#             */
+/*   Updated: 2022/06/23 13:57:59 by bbraga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE.H
+# define FT_GET_NEXT_LINE.H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
