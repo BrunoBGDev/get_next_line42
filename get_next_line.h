77fff3565f53c376256a14af57afbfa08502473e
BrunoBGDev <bruno.braga.design@gmail.com>
2022-06-28 15:37:48 -0300

    Update 0.0.1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:53:05 by bbraga            #+#    #+#             */
-/*   Updated: 2022/06/28 12:13:17 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/06/28 15:20:14 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:58:08 by bbraga            #+#    #+#             */
-/*   Updated: 2022/06/28 15:09:34 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/06/28 15:20:26 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+static void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned int	count;
 
@@ -37,7 +37,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	return ((unsigned char *)dst);
 }
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+static void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned int	count;
 
@@ -78,21 +78,21 @@ char	*ft_strdup(const char *s1)
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-		size_t	s1_len;
-		size_t	s2_len;
-		size_t	rst_len;
-		char	*join;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	rst_len;
+	char	*join;
 
-		if (!s1 || !s2)
-				return (0);
-		s1_len = ft_strlen((char *)s1);
-		s2_len = ft_strlen(s2);
-		rst_len = s1_len + s2_len + 1;
-		join = malloc(sizeof(char) * rst_len);
-		if (!join)
-				return (0);
-		ft_memmove(join, s1, s1_len);
-		ft_memmove(join, s1_len, s2_len);
-		join[rst_len - 1] = '\0';
-		return (join);
+	if (!s1 || !s2)
+		return (0);
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen(s2);
+	rst_len = s1_len + s2_len + 1;
+	join = malloc(sizeof(char) * rst_len);
+	if (!join)
+		return (0);
+	ft_memmove(join, s1, s1_len);
+	ft_memmove(join, s1_len, s2_len);
+	join[rst_len - 1] = '\0';
+	return (join);
 }
